d9c90923249e8a13ddc4b9b724761e66949e978e
sddhrthrt <sddhrthrt@gmail.com>
2014-02-16 12:13:10 +0530

    icons revamped

--- js/inci.js
+++ js/inci.js
@@ -28,13 +28,27 @@
         buildWall($("#top"), w, d, [-90, 0, 0], [0, d/2, -d/2]);
         buildWall($("#back"), w, h, [0, 0, 0], [0, 0, -d]);
     }
-    
+    function keepIcons(w, h, d){
+        var prefixes = '-moz- -webkit- -o- '.split(' '),
+            transf = '';
+        for(var i=0; i< 4; i++){
+            var el = $("#front"+i);
+            el.height(h/4).width(w);
+            for(var j=0,l=prefixes.length; j<l; j++){
+                var dist = ((-d/4)*i-(d/8));
+                el.css(prefixes[j] + 'transform', 
+                       "translateZ("+dist+"px) translateY(40px)");
+            }
+            console.log("("+i+") moving "+el.id+" "+((-d/4)*i-(d/8))+"px along z axis");
+        }
+    }
     $(document).ready(function () {
 	function adjust () {
 	    var w = $(window).width(),
 		h = $(window).height(),
 		d = Math.max(h, w);
 	    resizeCube(w, h, d);
+        keepIcons(w, h, d);
 	    return true;
 	}
 	$(window).resize(adjust);
