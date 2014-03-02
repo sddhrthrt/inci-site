71a6de3c1b42363d595132164c5bea5847521728
sddhrthrt <sddhrthrt@gmail.com>
2014-03-02 19:17:16 +0530

    started

--- server/serve.py
+++ server/serve.py
@@ -61,6 +61,13 @@ class User(db.Model):
     def __repr__(self):
         return "<User#%r: %r>"%(self.id, self.username)
 
+class Roles(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    role = db.Column(db.String(25))
+    def __repr__(self):
+        return "<Role#%r: %r>"%(self.id, self.role)
+
 class Confirmation(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
