75eebebafd5686e0b4b1fa6ea9058a396306ef62
sddhrthrt <sddhrthrt@gmail.com>
2014-03-02 18:58:33 +0530

    fixes

--- js/modal.js
+++ js/modal.js
@@ -47,7 +47,7 @@ function showModal(){
         var modalheight = $("#right-modal").height();
         var topheight = $(".modaltopmenu").height();
         var bottomheight = $(".modalbottommenu").height();
-        $("#right-modal div.content").height(modalheight-(topheight+bottomheight)-58);
+        $("#right-modal div.content").height(modalheight-(topheight+bottomheight)-8);
     }
 	$(window).resize(adjustModal);
     adjustModal();
