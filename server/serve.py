31e5cf936ef5200802ba4217c2227e81041c859d
sddhrthrt <sddhrthrt@gmail.com>
2014-03-03 18:14:34 +0530

    fb uniqueness

--- server/serve.py
+++ server/serve.py
@@ -38,7 +38,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(25), unique = True)
     email = db.Column(db.String(64), unique = True)
-    fb_username = db.Column(db.String(64), unique = True)
+    fb_username = db.Column(db.String(64))
     password = db.Column(db.String(128))
     phone = db.Column(db.String(25))
     name = db.Column(db.String(64))
