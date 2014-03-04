391cf97913578f63136f7e6199f6c15d2c09bf00
sddhrthrt <sddhrthrt@gmail.com>
2014-03-04 19:43:00 +0000

    working at ephemeral.me

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
-UPLOAD_FOLDER = '/home1/enginee8/public_html/inci-site-uploads/'
+UPLOAD_FOLDER = '/var/www/inci-site/inci-site-uploads/'
 ALLOWED_EXTENSIONS = set(['jpg', 'png'])
-LOG_FILE = "/home1/enginee8/public_html/inci-site-logs/log.log"
-SQLALCHEMY_DATABASE_URI = 'mysql://enginee8_inci:mysql-inci-user@localhost/enginee8_inci_site'
+LOG_FILE = "/home/sddhrthrt/inci-site/inci-site-logs/log.log"
+SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql-user-me@localhost/enginee8_inci_site'
 MAIL_SERVER = 'mail.incident.co.in'
 MAIL_PORT = 25
 MAIL_USERNAME = 'siddhartha@incident.co.in'
